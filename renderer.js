18b5018b0eba12486f638354fcc24faa3cb02751
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-12-13 16:24:53 +0100

    loading

--- renderer.js
+++ renderer.js
@@ -1,28 +1,20 @@
 const setButton = document.getElementById("btn");
 const setButton2 = document.getElementById("btn2");
-const loading = document.getElementById("loading");
+const loading = document.getElementById("text");
 // const titleInput = document.getElementById('title')
 setButton.addEventListener("click", () => {
-  loading.style.display = 'none';
+  loading.innerHTML = "Loading...";
 setButton2.style.display = 'none';
 setButton.style.display = 'none';
-setTimeout(() => {
-  // Hide loading sign
-  document.getElementById('loading').classList.add('hidden');
-}, 2000);
   //   const title = titleInput.value
   window.electronAPI.startPython();
 });
 
 setButton2.addEventListener("click", () => {
   //   const title = titleInput.value
-  loading.style.display = 'none';
+  loading.innerHTML = "Loading...";
 setButton2.style.display = 'none';
 setButton.style.display = 'none';
-setTimeout(() => {
-  // Hide loading sign
-  document.getElementById('loading').classList.add('hidden');
-}, 2000);
   window.electronAPI.startTalk();
 });
 
